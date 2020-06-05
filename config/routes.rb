ad3f72d9015a36b4e40f8295a64cb70e6d520088
Nao <dece.0.na.12@gmail.com>
2020-06-05 11:01:57 +0900

    体重管理アプリ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
  root 'graphs#index'
-end
+resource :graphs, only: %i[index create update]
+ end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,36 @@
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
 
+#データの入力範囲
+START_DATE = Date.today - 11.months
+END_DATE = Date.today + 1.months
+
+#1/(NO_RECORD_CONSTANT)の確率でデータを記録しない
+#記録が一部欠けていてもグラフが正常にかけるかをチェックするため
+NO_RECORD_CONSTANT = 5
+
+#記録する体重の範囲(乱数で値を決める都合上10倍しておく)
+MIN_WEIGHT = 600
+MAX_WEIGHT = 620
+DIV_CONSTANT = 10
+
 user = User.find_or_create_by!(email: EMAIL) do |user|
   
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました！'
 end
+
+user.graphs.destroy_all
+
+graphs = []
+(START_DATE..END_DATE).each do |date|
+  #1/(NO_RECORD_CONSTANT)の確率でデータをいれる操作をスキップ
+  next if rand(NO_RECORD_CONSTANT).zero?
+  graphs << {
+    user_id: user.id,
+    date: date,
+    weight: rand(MIN_WEIGHT..MAX_WEIGHT).to_f / DIV_CONSTANT
+  }
+end
+Graph.create!(graphs)
+puts '体重の初期データ投入に成功しました！'
